17877f496367c5699b87a92a675932464499d5cf
Sync-Faker <shangstephen011@gmail.com>
2024-12-16 19:58:44 +0800

    Add readme in pmf

--- Segmentation/lcps-main/scripts/val_kitty.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-
-nohup python val.py \
--c configs/pa_po_kitti_val.yaml \
--l kitti_val.log \
---resume \
-> nohup_kitti_val.log 2>&1 &
-
-
--- Segmentation/lcps-main/scripts/val_nusc.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-
-nohup python val.py -c configs/pa_po_nuscenes_val.yaml -l nusc_val_falsetime_onlyfilename_2.log --resume > nohup_nusc_val_falsetime_onlyfilename_2.log 2>&1 &
